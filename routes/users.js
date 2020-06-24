7dfce183527cf573e70b432b1766920542e11e08
abhinavsri360 <abhinav.022000@gmail.com>
2020-06-24 18:10:51 +0530

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('./cors')
+
+const Users = require('../models/user')
+
+var userRouter = express.Router()
+
+userRouter.use(bodyParser.json())
+
+userRouter.route('/')
+  .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200) })
+  .get(cors.cors, (req, res, next) => {
+    Users.find({})
+      .then((users) => {
+        res.statusCode = 200
+        res.setHeader('Content-type', 'application/json')
+        res.json(users)
+      }, (err) => next(err))
+      .catch((err) => next(err))
+  })
+  .post(cors.corsWithOptions, (req, res, next) => {
+    Users.create(req.body)
+      .then((user) => {
+        console.log(user)
+        res.statusCode = 200
+        res.setHeader('Content-type', 'application/json')
+        res.json(user)
+      }, (err) => next(err))
+      .catch((err) => next(err))
+  })
+
+module.exports = userRouter
